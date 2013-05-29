b7b628945fa4bb52422a8c7c89145cbe81e38e89
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-05-29 18:29:49 -0300

    lab02

--- lab002-val-cpf/flavio/val-cpf-test.js
+++ lab002-val-cpf/flavio/val-cpf-test.js
@@ -1,3 +1,84 @@
-test("hello test", function() {
-    ok(1 == "1", "Passed!");
+test("obj cpf - atribuindo", function() {
+    cpf.doc = "";
+    equal(false, cpf.validar(), "cumprimento zero não deve validar");
+
+    cpf.doc = 0;
+    equal(false, cpf.validar(), "zero não deve validar");
+
+    cpf.doc = null;
+    equal(false, cpf.validar(), "nulo não deve validar");
+
+    cpf.doc = "0000000";
+    equal(false, cpf.validar(), "0000000 não deve validar");
+});
+test("obj cpf - tamanho", function() {
+
+    cpf.doc = "123456789123"; // string length 12
+    equal(false, cpf.validar(), "Cumprimento do CPF não deve ser diferente de 11");
+
+    cpf.doc = "1234567891"; // string length 10
+    equal(false, cpf.validar(), "Cumprimento do CPF não deve ser diferente de 11");
+
+    cpf.doc = "123.456.789-999"; // string length 14 - 3
+    equal(false, cpf.validar(), "Cumprimento do CPF não deve ser diferente de 11");
+
+});
+test("obj cpf - extrair_apenas_numeros()", function() {
+
+    cpf.doc = "123.456.789-12";
+    cpf.extrair_apenas_numeros();
+    equal("12345678912", cpf.doc, "Deve conter apenas números");
+
+    cpf.doc = "1.-/?;:.,><][}{~^'`'\"2!@#$%*()-_=+-*|";
+    cpf.extrair_apenas_numeros();
+    equal("12", cpf.doc, "Deve conter apenas números");
+
+});
+test("obj cpf - extrair_digitos()", function() {
+
+    cpf.doc = "123.456.789-09";
+    cpf.extrair_apenas_numeros();
+    cpf.extrair_digitos();
+    equal("0", cpf.digt1.num, "Extração do dígito 1 falhou");
+    equal("9", cpf.digt2.num, "Extração do dígito 2 falhou");
+
+});
+test("obj cpf - mult_somar_cpf()", function() {
+
+    cpf.doc = "123.456.789-09";
+    cpf.extrair_apenas_numeros();
+    cpf.digt1.mult_somar_cpf();
+    equal("210", cpf.digt1.soma, "Soma do dígito 1 não bate");
+
+    cpf.digt2.mult_somar_cpf();
+    equal("255", cpf.digt2.soma, "Soma do dígito 2 não bate");
+
+
+});
+test("obj cpf - soma_calc_resto()", function() {
+    cpf.doc = "123.456.789-09";
+    cpf.extrair_apenas_numeros();
+
+    cpf.digt1.mult_somar_cpf();
+    cpf.digt1.resto = cpf.calc_resto(cpf.digt1.soma);
+    equal("0", cpf.digt1.resto, "O resto do dígito 1 não está batendo");
+
+    cpf.digt2.mult_somar_cpf();
+    cpf.digt2.resto = cpf.calc_resto(cpf.digt2.soma);
+    equal("9", cpf.digt2.resto, "O resto do dígito 2 não está batendo");
+});
+test("obj cpf - validar_digito()", function() {
+
+    cpf.doc = "123.456.789-09";
+    cpf.extrair_apenas_numeros();
+    cpf.extrair_digitos();
+
+    cpf.digt1.mult_somar_cpf();
+    cpf.digt1.resto = cpf.calc_resto(cpf.digt1.soma);
+    ok(cpf.validar_digito(cpf.digt1.resto, cpf.digt1.num), "Dígito 1 inválido, lógo, cpf inválido");
+
+    cpf.digt2.mult_somar_cpf();
+    cpf.digt2.resto = cpf.calc_resto(cpf.digt2.soma);
+    ok(cpf.validar_digito(), "Dígito 2 inválido, lógo, cpf inválido");
+
 });
--- lab002-val-cpf/flavio/val-cpf.js
+++ lab002-val-cpf/flavio/val-cpf.js
@@ -1,6 +1,105 @@
-// Referencias:
-//http://www.devmedia.com.br/validar-cpf-com-javascript/23916
-//http://www.gerardocumentos.com.br/?pg=funcao-javascript-para-validar-cpf    
+var cpf = {
+    doc: "",
+    digt1: {
+        num: "",
+        resto: "",
+        soma: "",
+        mult_somar_cpf: function() {
+
+            var cpf_arr = cpf.doc.split("");
+
+            this.soma = 0;
+            this.soma += cpf_arr[0] * 10;
+            this.soma += cpf_arr[1] * 9;
+            this.soma += cpf_arr[2] * 8;
+            this.soma += cpf_arr[3] * 7;
+            this.soma += cpf_arr[4] * 6;
+            this.soma += cpf_arr[5] * 5;
+            this.soma += cpf_arr[6] * 4;
+            this.soma += cpf_arr[7] * 3;
+            this.soma += cpf_arr[8] * 2;
+        }
+    },
+    digt2: {
+        num: "",
+        resto: "",
+        soma: "",
+        mult_somar_cpf: function() {
+
+            var cpf_arr = cpf.doc.split("");
+
+            this.soma = 0;
+            this.soma += cpf_arr[0] * 11;
+            this.soma += cpf_arr[1] * 10;
+            this.soma += cpf_arr[2] * 9;
+            this.soma += cpf_arr[3] * 8;
+            this.soma += cpf_arr[4] * 7;
+            this.soma += cpf_arr[5] * 6;
+            this.soma += cpf_arr[6] * 5;
+            this.soma += cpf_arr[7] * 4;
+            this.soma += cpf_arr[8] * 3;
+            this.soma += cpf_arr[9] * 2;
+        }
+    },
+    validar: function() {
+        "use strict";
+
+        if (!this.doc) {
+            return false;
+        }
+
+        if (this.doc === "0000000") {
+            return false;
+        }
+
+        this.extrair_apenas_numeros();
+        this.extrair_digitos();
+
+        if (this.doc.length != 11) {
+            return false;
+        }
+
+        this.digt1.mult_somar_cpf();
+        this.digt1.resto = this.calc_resto(this.digt1.soma);
+        if (!cpf.validar_digito(this.digt1.resto, this.digt1.num)){
+            return false;
+        }
+
+        this.digt2.mult_somar_cpf();
+        this.digt2.resto = this.calc_resto(this.digt2.soma);
+        if (!cpf.validar_digito(this.digt2.resto, this.digt2.num)){
+            return false;
+        }
+
+        return true;
+    },
+    extrair_apenas_numeros: function() {
+        this.doc = this.doc.replace(/[^\d]+/g, '');
+    },
+    extrair_digitos: function() {
+        var cpf_arr = this.doc.split("");
+        this.digt1.num = cpf_arr[9];
+        this.digt2.num = cpf_arr[10];
+    },
+    calc_resto: function(soma) {
+        var resto = (soma * 10) % 11;
+
+        if ((resto == 10) || (resto == 11))
+            resto = 0;
+
+        return resto;
+    },
+    validar_digito: function (resto, digito) {
+        if (resto == digito)
+            return true;
+    }
+
+};
+
+
+
+
+
 var validarCPF = function() {
     var me = this,
             CPF,
@@ -16,12 +115,12 @@ var validarCPF = function() {
     // Codifica o valor do CPF para ser apenas um valor numérico
     CPF = me.txtCPFtemp.val().replace(/[^\d]+/g, '');
 
-    // Verifica se o tamanho do CPF contém mais que 11 caracteres 
+    // Verifica se o tamanho do CPF contém mais que 11 caracteres
     if (CPF.length != 11) {
         validacaoInvalida = true;
     }
 
-    // Verifica se a sequência de digitos são identicos 
+    // Verifica se a sequência de digitos são identicos
     for (i = 0; i < CPF.length - 2; i++) {
         if (CPF.charAt(i) != CPF.charAt(i + 1)) {
             digitosIguais = 0;
