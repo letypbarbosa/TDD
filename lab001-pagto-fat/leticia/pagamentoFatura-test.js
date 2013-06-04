cfea621d169b836fb8cc7d1f2add8c2463f4f59e
Letícia <leticia_sakura@yahoo.com.br>
2013-06-04 17:31:52 -0300

    arquivos

--- lab001-pagto-fat/leticia/pagamentoFatura-test.js
+++ lab001-pagto-fat/leticia/pagamentoFatura-test.js
@@ -1,6 +1,10 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+test("statusData", function() {
+    
+    dtPagamento  = "10/06/2013";
+    dtVencimento = "20/06/2013";
+    
+    result = situacaoPagamento.CalcularData( dtPagamento, dtVencimento );
+    
+    equal( result, dataPagamento, "Pagamento em dia" );
+    
+});
--- lab001-pagto-fat/leticia/pagamentoFatura.js
+++ lab001-pagto-fat/leticia/pagamentoFatura.js
@@ -1,6 +1,13 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+situacaoPagamento = {
+    statusData: function(){
+    },
+    
+    CalcularData: function( dtPagamento, dtVencimento){
 
-
+        var dataPagamento = dtPagamento.replace('/', '' );
+        
+    }
+            
+            
+    
+};
