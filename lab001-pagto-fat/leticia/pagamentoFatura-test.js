5a8e53c1a34934a323f0106eefd7393f95f9e9e7
Letícia <leticia_sakura@yahoo.com.br>
2013-06-05 10:47:26 -0300

    Enviando o test

--- lab001-pagto-fat/leticia/pagamentoFatura-test.js
+++ lab001-pagto-fat/leticia/pagamentoFatura-test.js
@@ -1,24 +1,31 @@
-test( "statusPagamento", function(){
+test("verificaPagtoAtrasado", function() {
     
-        // Efetuado no dia do vencimento
-        dtPagamento  = "25/06/2013";
-        dtVencimento = "25/06/2013";
+    dt_vencto = "25/11/2010";
+    dt_pagto  = "30/11/2010";
+    resp      = Fatura.verificaPagto( dt_vencto, dt_pagto );
+    equal("Pagto feito com atraso de 5 dias", resp, "pagto deve ter 5 dias de atraso");
 
-        retorno = pagamentoFatura.statusPagamento( dtPagamento, dtVencimento );
-        equal( "efetuadoVencimento" , retorno, "Pagamento efetuado no dia do vencimento");
+    dt_vencto = "10/04/2011";
+    dt_pagto  = "10/05/2011";
+    resp = Fatura.verificaPagto(dt_vencto, dt_pagto);
+    equal("Pagto feito com atraso de 30 dias", resp, "pagto deve ter 30 dias de atraso");
 
-        // Efetuando com Antecedência
-        dtPagamento  = "20/06/2013";
-        dtVencimento = "25/06/2013";
+});
 
-        retorno = pagamentoFatura.statusPagamento( dtPagamento, dtVencimento );
-        equal( "efetuadoAndecendencia" , retorno, "Pagamento efetuado com 5 dias de antecedência");
+test("pgtoEmDias", function() {
 
-        // Efetuado com Atraso
-        dtPagamento  = "30/06/2013";
-        dtVencimento = "25/06/2013";
+    dt_vencto = "01/11/2010";
+    dt_pagto  = "01/11/2010";
+    resp = Fatura.verificaPagto(dt_vencto, dt_pagto);
+    equal("Pagto feito em dia", resp, "pagto deve ter sido feito em dia");
 
-        retorno = pagamentoFatura.statusPagamento( dtPagamento, dtVencimento );
-        equal( "efetuadoAtraso" , retorno, "Pagamento efetuado com 5 dias de atraso");
-    
+});
+
+test("pgtoEmUmDiaAtrasado", function() {
+
+    dt_vencto = "27/12/2010";
+    dt_pagto  = "28/12/2010";
+    resp = Fatura.verificaPagto(dt_vencto, dt_pagto);
+    equal("Pagto feito com atraso de 1 dia", resp, "pagto realizado com atraso de um dia");
+
 });
