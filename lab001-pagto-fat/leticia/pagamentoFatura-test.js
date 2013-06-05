bb5a588ce06275c39d2f887fc4b7fcf449c6aebb
Leticia <leticia@localhost.localdomain>
2013-06-04 22:50:10 -0300

    Efetuando o TDD

--- lab001-pagto-fat/leticia/pagamentoFatura-test.js
+++ lab001-pagto-fat/leticia/pagamentoFatura-test.js
@@ -1,10 +1,24 @@
-test("statusData", function() {
+test( "statusPagamento", function(){
     
-    dtPagamento  = "10/06/2013";
-    dtVencimento = "20/06/2013";
-    
-    result = situacaoPagamento.CalcularData( dtPagamento, dtVencimento );
-    
-    equal( result, dataPagamento, "Pagamento em dia" );
+        // Efetuado no dia do vencimento
+        dtPagamento  = "25/06/2013";
+        dtVencimento = "25/06/2013";
+
+        retorno = pagamentoFatura.statusPagamento( dtPagamento, dtVencimento );
+        equal( "efetuadoVencimento" , retorno, "Pagamento efetuado no dia do vencimento");
+
+        // Efetuando com Antecedência
+        dtPagamento  = "20/06/2013";
+        dtVencimento = "25/06/2013";
+
+        retorno = pagamentoFatura.statusPagamento( dtPagamento, dtVencimento );
+        equal( "efetuadoAndecendencia" , retorno, "Pagamento efetuado com 5 dias de antecedência");
+
+        // Efetuado com Atraso
+        dtPagamento  = "30/06/2013";
+        dtVencimento = "25/06/2013";
+
+        retorno = pagamentoFatura.statusPagamento( dtPagamento, dtVencimento );
+        equal( "efetuadoAtraso" , retorno, "Pagamento efetuado com 5 dias de atraso");
     
 });
--- lab001-pagto-fat/leticia/pagamentoFatura.js
+++ lab001-pagto-fat/leticia/pagamentoFatura.js
@@ -1,13 +1,36 @@
-situacaoPagamento = {
-    statusData: function(){
-    },
+pagamentoFatura = {
     
-    CalcularData: function( dtPagamento, dtVencimento){
-
-        var dataPagamento = dtPagamento.replace('/', '' );
+    statusPagamento: function( dtPagamento, dtVencimento ){
         
+        // Transformar a data em uma array
+        var arr_dtPagamento  = dtPagamento.split('/');
+            arr_dtVencimento = dtVencimento.split('/');
+        
+            // Soma a data de pagamento
+            soma_pagamento  = parseInt( arr_dtPagamento[0] ) +
+                              parseInt( arr_dtPagamento[1] ) + 
+                              parseInt( arr_dtPagamento[2] );
+                          
+                          
+            // Soma a data de vencimento             
+            soma_vencimento = parseInt( arr_dtVencimento[0] ) +
+                              parseInt( arr_dtVencimento[1] ) + 
+                              parseInt( arr_dtVencimento[2] );     
+                         
+                         
+            // Retornar o status do pagamento 
+            if ( soma_vencimento == soma_pagamento ){ 
+                
+                return "efetuadoVencimento"; 
+                
+            } else if ( soma_vencimento > soma_pagamento ){ 
+                
+                return "efetuadoAndecendencia"; 
+                
+            } else if ( soma_vencimento < soma_pagamento ) {
+                
+               return "efetuadoAtraso";  
+            }
     }
-            
-            
     
-};
+}
