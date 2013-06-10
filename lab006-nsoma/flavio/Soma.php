16069372f4e5dac07338512fdea62688d55862fd
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-06-10 13:39:17 -0300

    resolvido

--- lab006-nsoma/flavio/Soma.php
+++ lab006-nsoma/flavio/Soma.php
@@ -1,7 +1,19 @@
 <?php
 
 class Soma {
-    
+
+    function resultado($n) {
+
+        $res = 0;
+
+        for ($i = 0; $i < $n; $i++) {
+            $_n = $i+1;
+            $res += ($_n + 1) / (pow($_n, 2));
+        }
+
+        return $res;
+    }
+
 }
 
 ?>
