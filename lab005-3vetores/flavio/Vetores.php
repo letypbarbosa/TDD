c91bc9e0b1363d1c9702eccd88adbc41c7b92c54
Flávio Micheletti <flaviomicheletti@hotmail.com>
2013-06-10 12:44:14 -0300

    resolvido

--- lab005-3vetores/flavio/Vetores.php
+++ lab005-3vetores/flavio/Vetores.php
@@ -2,6 +2,147 @@
 
 class Vetores {
 
+    private $vetor30;
+    private $vetor20;
+
+    /**
+     * Vetor30
+     */
+    function setVetorTrinta($valor = null) {
+        if ($valor) {
+            $this->vetor30 = $valor;
+        } else {
+            for ($i = 0; $i < 30; $i++) {
+                $this->vetor30[] = rand(1, 100);
+            }
+        }
+    }
+
+    function getVetorTrinta() {
+        return $this->vetor30;
+    }
+
+    /**
+     * vetor 20
+     */
+    function setVetorVinte($valor = null) {
+        if ($valor) {
+            $this->vetor20 = $valor;
+        } else {
+            for ($i = 0; $i < 20; $i++) {
+                $this->vetor20[] = rand(1, 100);
+            }
+        }
+    }
+
+    function getVetorVinte() {
+        return $this->vetor20;
+    }
+
+    /**
+     *
+     * @param type $param
+     * @return boolean
+     */
+    function eImpar($param) {
+
+        if (($param % 2 ) != 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     * @param type $param
+     * @return boolean
+     */
+    function ePar($param) {
+
+        if (($param % 2 ) == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     * @param type $param
+     * @return boolean
+     */
+    function ePrimo($param) {
+
+        if ($param == 0 || $param == 1) {
+            return false;
+        }
+
+        for ($i = ($param - 1); $i > 1; $i--) {
+            if (($param % $i) == 0) {
+                return false;
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    function getRespPrimeiroVetor() {
+
+        $resp = array();
+
+        foreach ($this->vetor30 as $valor) {
+            if ($this->ePar($valor)) {
+                $resp[] = $valor;
+            }
+        }
+
+        return $resp;
+    }
+
+    
+    /**
+     * 
+     * @return type
+     */
+    function getRespSegundoVetor() {
+
+        $resp = array();
+
+        foreach ($this->vetor20 as $valor) {
+            if ($this->eImpar($valor)) {
+                $resp[] = $valor;
+            }
+        }
+
+        return $resp;
+    }    
+    
+    
+    
+    function getRespTerceiroVetor() {
+        $resp = array();
+
+        foreach ($this->vetor30 as $valor) {
+            if ($this->ePrimo($valor)) {
+                $resp[] = $valor;
+            }
+        }
+        
+        foreach ($this->vetor20 as $valor) {
+            if ($this->ePrimo($valor)) {
+                $resp[] = $valor;
+            }
+        }
+
+        return $resp;        
+    }
+    
 }
 
 ?>
--- lab005-3vetores/flavio/VetoresTest.php
+++ lab005-3vetores/flavio/VetoresTest.php
@@ -15,89 +15,101 @@ class VetoresTest extends PHPUnit_Framework_Testcase {
         $vetores->setVetorVinte();
         $resp = count($vetores->getVetorVinte());
         $this->assertEquals(20, $resp);
+        
+        $vetores->setVetorTrinta(array(1, 2, 3));
+        $resp = count($vetores->getVetorTrinta());
+        $this->assertEquals(3, $resp);
+
+        $vetores->setVetorVinte(array(1, 2, 3));
+        $resp = count($vetores->getVetorVinte());
+        $this->assertEquals(3, $resp);        
+        
     }
 
-//    function test_eImpar() {
-//        $vetores = new Vetores();
-//        
-//        $this->assertTrue($vetores->eImpar(1));
-//        $this->assertTrue($vetores->eImpar(3));
-//        $this->assertTrue($vetores->eImpar(5));
-//        $this->assertTrue(!$vetores->eImpar(6));
-//        $this->assertTrue(!$vetores->eImpar(7));
-//        
-//    }
-//
-//    function test_ePar() {
-//        $vetores = new Vetores();
-//        
-//        $this->assertTrue(!$vetores->ePar(1));
-//        $this->assertTrue($vetores->ePar(2));
-//        $this->assertTrue(!$vetores->ePar(5));
-//        $this->assertTrue($vetores->ePar(6));
-//        $this->assertTrue(!$vetores->ePar(7));
-//        
-//    }
-//
-//    function test_ePrimo() {
-//        $vetores = new Vetores();
-//                
-//        $this->assertTrue($vetores->ePrimo(2));
-//        $this->assertTrue($vetores->ePrimo(3));
-//        $this->assertTrue($vetores->ePrimo(5));
-//        $this->assertTrue($vetores->ePrimo(7));
-//        $this->assertTrue($vetores->ePrimo(11));
-//        $this->assertTrue($vetores->ePrimo(13));
-//        $this->assertTrue($vetores->ePrimo(17));
-//        $this->assertTrue($vetores->ePrimo(19));
-//        $this->assertTrue($vetores->ePrimo(23));
-//        $this->assertTrue($vetores->ePrimo(29));
-//        $this->assertTrue($vetores->ePrimo(31));
-//        $this->assertTrue($vetores->ePrimo(37));
-//        $this->assertTrue($vetores->ePrimo(41));
-//
-//        $this->assertTrue(!$vetores->ePrimo(0));
-//        $this->assertTrue(!$vetores->ePrimo(1));
-//        $this->assertTrue(!$vetores->ePrimo(3));
-//        $this->assertTrue(!$vetores->ePrimo(4));
-//        
-//    }
-//
-//    function testPrimeiroVetorPar() {
-//        $vetores = new Vetores();
-//        $vetores->setVetorTrinta(array(6, 7, 21));
-//        $vetores->setVetorVinte(array(11, 12, 13));
-//
-//        $arr = $vetores->getRespPrimeiroVetor();
-//
-//        $this->assertEquals(6, $arr[0]);
-//        
-//    }
-//
-//    function testSegundoVetorImpar() {
-//
-//        $vetores = new Vetores();
-//        $vetores->setVetorTrinta(array(6, 7, 21));
-//        $vetores->setVetorVinte(array(11, 12, 13));
-//
-//        $arr = $vetores->getRespSegundoVetor();
-//
-//        $this->assertEquals(11, $arr[0]);
-//        $this->assertEquals(13, $arr[1]);
-//    }
-//
-//    function testTerceiroVetorPrimos() {
-//
-//        $vetores = new Vetores();
-//        $vetores->setVetorTrinta(array(6, 7, 21));
-//        $vetores->setVetorVinte(array(11, 12, 13));
-//
-//        $arr = $vetores->getRespTerceiroVetor();
-//
-//        $this->assertEquals(7, $arr[0]);
-//        $this->assertEquals(11, $arr[1]);
-//        $this->assertEquals(13, $arr[2]);
-//    }
+    function test_eImpar() {
+        $vetores = new Vetores();
+        
+        $this->assertTrue($vetores->eImpar(1));
+        $this->assertTrue($vetores->eImpar(3));
+        $this->assertTrue($vetores->eImpar(5));
+        $this->assertTrue(!$vetores->eImpar(6));
+        $this->assertTrue($vetores->eImpar(7));
+        
+    }
+
+    function test_ePar() {
+        $vetores = new Vetores();
+        
+        $this->assertTrue(!$vetores->ePar(1));
+        $this->assertTrue($vetores->ePar(2));
+        $this->assertTrue(!$vetores->ePar(5));
+        $this->assertTrue($vetores->ePar(6));
+        $this->assertTrue(!$vetores->ePar(7));
+        
+    }
+
+    function test_ePrimo() {
+        $vetores = new Vetores();
+                
+        $this->assertTrue($vetores->ePrimo(2));
+        $this->assertTrue($vetores->ePrimo(3));
+        $this->assertTrue($vetores->ePrimo(5));
+        $this->assertTrue($vetores->ePrimo(7));
+        $this->assertTrue($vetores->ePrimo(11));
+        $this->assertTrue($vetores->ePrimo(13));
+        $this->assertTrue($vetores->ePrimo(17));
+        $this->assertTrue($vetores->ePrimo(19));
+        $this->assertTrue($vetores->ePrimo(23));
+        $this->assertTrue($vetores->ePrimo(29));
+        $this->assertTrue($vetores->ePrimo(31));
+        $this->assertTrue($vetores->ePrimo(37));
+        $this->assertTrue($vetores->ePrimo(41));
+
+        $this->assertTrue(!$vetores->ePrimo(0));
+        $this->assertTrue(!$vetores->ePrimo(1));
+        $this->assertTrue(!$vetores->ePrimo(4));
+        $this->assertTrue(!$vetores->ePrimo(6));
+        $this->assertTrue(!$vetores->ePrimo(8));
+        $this->assertTrue(!$vetores->ePrimo(9));
+        $this->assertTrue(!$vetores->ePrimo(10));
+        
+    }
+
+    function testPrimeiroVetorPar() {
+        $vetores = new Vetores();
+        $vetores->setVetorTrinta(array(6, 7, 21));
+        $vetores->setVetorVinte(array(11, 12, 13));
+
+        $arr = $vetores->getRespPrimeiroVetor();
+
+        $this->assertEquals(6, $arr[0]);
+        
+    }
+
+    function testSegundoVetorImpar() {
+
+        $vetores = new Vetores();
+        $vetores->setVetorTrinta(array(6, 7, 21));
+        $vetores->setVetorVinte(array(11, 12, 13));
+
+        $arr = $vetores->getRespSegundoVetor();
+
+        $this->assertEquals(11, $arr[0]);
+        $this->assertEquals(13, $arr[1]);
+    }
+
+    function testTerceiroVetorPrimos() {
+
+        $vetores = new Vetores();
+        $vetores->setVetorTrinta(array(6, 7, 21));
+        $vetores->setVetorVinte(array(11, 12, 13));
+
+        $arr = $vetores->getRespTerceiroVetor();
+
+        $this->assertEquals(7, $arr[0]);
+        $this->assertEquals(11, $arr[1]);
+        $this->assertEquals(13, $arr[2]);
+    }
 
 }
 
